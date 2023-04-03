7ba324f5ad8f95144528c77238f048fbcbdd7baa
Firepal <badbrad@outlook.fr>
2023-04-03 03:20:59 +0200

    Fix Python 3.10.10 'array' quirk

--- decay_cache.py
+++ decay_cache.py
@@ -1,57 +1,56 @@
 import gc
 
-class Frame:
-    frame: bytes = None
+class DecayItem:
+    item = None
     timer: int = 0
 
     def __init__(self,
-                 frame: bytes = None,
+                 item = None,
                  timer: int = 0):
-        self.frame = frame
+        self.item = item
         self.timer = timer
 
 class DecayCache:
-    array: dict[Frame] = {}
+    items: dict[DecayItem] = {}
     decay: int = 100
-    bad_frames = 0
 
     def __init__(self,size: int):
         for i in range(size):
-            self.array[i] = Frame()
+            self.items[i] = DecayItem()
     
     def reinit(self):
-        self.__init__(len(self.array))
+        self.__init__(len(self.items))
     
     def item_decayed(self, i: int):
-        return self.array[i].timer == 0
+        return self.items[i].timer == 0
 
     def item_usable(self, i: int):
-        return self.array[i].frame != None
+        return self.items[i].item != None
     
     def __set_timer(self,i: int,time: int):
-        self.array[i].timer = time
+        self.items[i].timer = time
     
     def reset_timer(self,i: int):
         self.__set_timer(i,self.decay)
     
     def process(self):
-        self.decayed_frames = 0
-        for i in range(len(self.array)):
-            self.array[i].timer = max(0,self.array[i].timer-1)
+        self.decayed_items = 0
+        for i, _ in enumerate(self.items):
+            self.items[i].timer = max(0,self.items[i].timer-1)
             
             if self.item_decayed(i):
-                self.decayed_frames += 1
-                self.array[i] = Frame()
+                self.decayed_items += 1
+                self.items[i] = DecayItem()
         
         # free decayed items from memory
         gc.collect()
     
 
-    def clear(self,frame_ids: list[int]):
-        for id in frame_ids:
-            if self.item_usable(id):
-                self.reset_timer(id)
+    def clear(self,ids: list[int]):
+        for item_id in ids:
+            if self.item_usable(item_id):
+                self.reset_timer(item_id)
     
-    def set_frame(self,i: int,frame: bytes,):
-        self.array[i] = Frame(frame,self.decay)
+    def set_item(self,i: int,item):
+        self.items[i] = DecayItem(item,self.decay)
     
